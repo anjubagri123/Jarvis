42075eb86f81a7735465fde587cb093185dabbb0
anjubagri123 <60001007+anjubagri123@users.noreply.github.com>
2025-03-28 01:16:01 +0530

    Some Improvements i have done in code

--- jarvis.py
+++ jarvis.py
@@ -1,15 +1,23 @@
 import datetime
 import smtplib
-import pyttsx3          #pip install pyttsx3
-import speech_recognition as sr        #pip install speechRecognition
-import wikipedia                      #pip install wikipedia
+import pyttsx3  # pip install pyttsx3
+import speech_recognition as sr  # pip install speechRecognition
+import wikipedia  # pip install wikipedia
 import webbrowser
 import os
+import random
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-#print(voices[1].id)
-engine.setProperty('voice',voices[1].id)  # 1 for girl's voice and 0 for boy's voice
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)  # 1 for girl's voice and 0 for boy's voice
+
+receiver_dict = {
+    "Anju": "bagrianju2002@gmail.com",
+    "Anoop": "anoopbagri7@gmail.com",
+    "Jasvir": "jasvirsinghbagri@gmail.com",
+    "Dilbag": "dilbagsinghbagri@gmail.com"
+}
 
 
 def speak(audio):
@@ -41,33 +49,35 @@ def takeCommand():
         print("Recognizing....")
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}\n")
+
     except Exception as e:
-       # print(e)
+        # print(e)
         print("Say that again please...")
         return "None"
     return query
 
+
 def sendEmail(to, content):
-    server= smtplib.SMTP('smtp.gmail.com',587)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('bagrianju2002@gmail.com','****')
-    server.sendmail('bagrianju2002@gmail.com',to, content)
+    f = open(r"C:\Users\bagri\OneDrive\Desktop\Resume Projects\Jarvis-main\gmail_pass", "r")
+    # print(f.read())
+    server.login('bagrianju2002@gmail.com', f.read())
+    server.sendmail('bagrianju2002@gmail.com', to, content)
     server.close()
 
 
-
 if __name__ == '__main__':
-    #speak("Anju is very good girl") #  here we already given the content which we want to listen from javris.
+    # speak("Anju is very good girl") #  here we already given the content which we want to listen from javris.
     wishMe()
     while True:
-   # if 1:
         query = takeCommand().lower()
-    #logic for execute tasks based on query
+        # logic for execute tasks based on query
         if 'wikipedia' in query:
             speak("Searching Wikipedia....")
-            query = query.replace("wikipedia","")
-            results= wikipedia.summary(query, sentences=2)
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=1)
             speak("According to wikipedia")
             print(results)
             speak(results)
@@ -80,30 +90,42 @@ if __name__ == '__main__':
         elif 'open linkedin' in query:
             webbrowser.open("linkedin.com")
 
+        elif 'open netflix' in query:
+            webbrowser.open("netflix.com")
+
+        elif 'open amazon' in query:
+            webbrowser.open("amazon.com")
+
         elif 'play music' in query:
-            music_dir= 'D:\\music_dir'
-            songs= os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[1]))
+            music_dir = "D:\Songs"
+            songs = os.listdir(music_dir)
+            x = random.randint(1, 12)
+            # print(songs)
+            os.startfile(os.path.join(music_dir, songs[x]))
 
         elif 'the time' in query:
-            strTime =datetime.datetime.now().strftime("%H:%M:%S")
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Mam, The time is {strTime}")
 
         elif 'open code' in query:
-            codePath="C:\\Users\\Dell\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = r"C:\Users\bagri\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk"
             os.startfile(codePath)
 
-   #     elif 'send email to anju' in query:
-      #      try:
 
-     #           speak("What should i say?")
-     #           content = takeCommand()
-     #           to = "bagrianju2002@gmail.com"
-     #           sendEmail(to, content)
-     #           speak("Email has been send!")
-     #       except Exception as e:
-      #          print(e)
-       #         speak("sorry my sister i am not able to send your mail at this moment")
-#if 'quit jarvis' in query:
-   # quit()
+        elif 'send email to anju' in query:
+            try:
+
+                speak("What should i say?")
+                content = takeCommand()
+                # to = "bagrianju2002@gmail.com"
+                to = receiver_dict.get("Anju")
+                sendEmail(to, content)
+                speak("Email has been send!")
+            except Exception as e:
+                print(e)
+                speak("sorry my sister i am not able to send your mail at this moment")
+        if 'quit jarvis' in query:
+            speak("Quitting Ma'am .Thanks for your time ")
+            Ack_Msg = "Quitting Ma'am .Thanks for your time "
+            print("Ack_Msg: ", Ack_Msg)
+            quit()
